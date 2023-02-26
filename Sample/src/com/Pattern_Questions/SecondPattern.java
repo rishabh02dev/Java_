4b3542fc82d13bbac71b67620d664a1094f56b0b
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-02-26 14:26:39 +0530

    Pattern 2 solved

--- Sample/src/com/Pattern_Questions/SecondPattern.java
+++ Sample/src/com/Pattern_Questions/SecondPattern.java
@@ -2,9 +2,17 @@ package com.Pattern_Questions;
 
 public class SecondPattern {
     public static void main(String[] args) {
-        pattern2();
+        pattern2(5);
     }
+
     static void pattern2(int n){
-        
+        for (int row = 1; row<=n ; row++){
+            //for every row , run the col
+            for (int col = 1; col<=n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }

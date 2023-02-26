5e53f7080f711e5d927ecec3143da097966df156
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-02-26 19:41:22 +0530

    Pattern Problem Soving

---
+++ Sample/src/com/Pattern_Questions/Pattern30.java
@@ -0,0 +1,23 @@
+package com.Pattern_Questions;
+
+public class Pattern30 {
+    public static void main(String[] args) {
+        Pattern30(5);
+    }
+
+    static void Pattern30(int n){
+        for (int row = 1; row<=n ; row++){
+            for (int space = 0; space<n-row; space++){
+                System.out.print("  ");
+            }
+            for (int col = row; col>=1; col--){
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col<=row; col++){
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ Sample/src/com/Pattern_Questions/Pattern31.java
@@ -0,0 +1,18 @@
+package com.Pattern_Questions;
+
+public class Pattern31 {
+    public static void main(String[] args) {
+        pattern31(4);
+    }
+    static void pattern31(int n){
+        n = 2*n;
+        for (int row = 0; row<=n; row++){
+            for (int col = 0; col<=n ; col++){
+                int atEveryIndex = Math.min(Math.min(row,col), Math.min(n-row, n-col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
